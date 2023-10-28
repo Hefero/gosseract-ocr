d037d990abc2710903f7078b2c77a8b4d17605aa
Hefero <guilherme.marques00@hotmail.com>
2023-10-28 18:10:04 -0300

    lib

--- tesseract.cmd
+++ tesseract.cmd
@@ -1,2 +1,4 @@
 tesseract input.png output -l eng --oem 2 -c tessedit_create_hocr=1 -c tessedit_create_alto=1 -c tessedit_create_txt=1 -c hocr_font_info=0
 tesseract --print-parameters
+
+# install tesseract with trained languages from https://github.com/UB-Mannheim/tesseract/wiki
