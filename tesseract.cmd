d52f48d6a5176858ee2248e60216b128f39ac2a7
Hefero <guilherme.marques00@hotmail.com>
2023-10-28 18:07:13 -0300

    output cmd

--- tesseract.cmd
+++ tesseract.cmd
@@ -1,2 +1,2 @@
-tesseract input.png output -l eng --oem 2 -c tessedit_create_hocr=1 -c hocr_font_info=0
-tesseract input.png output -l eng --oem 2 -c tessedit_create_hocr=0
+tesseract input.png output -l eng --oem 2 -c tessedit_create_hocr=1 -c tessedit_create_alto=1 -c tessedit_create_txt=1 -c hocr_font_info=0
+tesseract --print-parameters
