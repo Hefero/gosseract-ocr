008a4995478b033bae6bc4af10278be45ec5a506
Hefero <guilherme.marques00@hotmail.com>
2023-10-28 20:30:47 -0300

    lastread

--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"os"
 
 	"github.com/otiai10/gosseract/v2"
 )
@@ -12,5 +14,24 @@ func main() {
 	client.SetImage("input.png")
 	text, _ := client.Text()
 	fmt.Println(text)
+	f, err := os.Create("lastread.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+	_, err2 := f.WriteString(text)
+	if err2 != nil {
+		log.Fatal(err2)
+	}
+	HOCRText, _ := client.HOCRText()
+	f2, err := os.Create("lastread.hocr")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f2.Close()
+	write, err := f2.WriteString(HOCRText)
+	if err != nil {
+		log.Fatal(write)
+	}
 	// Hello, World!
 }
